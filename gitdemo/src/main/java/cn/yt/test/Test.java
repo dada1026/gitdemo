9f9cca0fbe32b9e29359eb2b948aab43f04a59e2
denghongda <1990995390@qq.com>
2023-07-15 23:22:08 +0800

    hi

--- gitdemo/src/main/java/cn/yt/test/Test.java
+++ gitdemo/src/main/java/cn/yt/test/Test.java
@@ -3,6 +3,7 @@ package cn.yt.test;
 public class Test {
     public void hello(){
         System.out.println("hello");
+        System.out.println("12312");
     }
 
     public static void main(String[] args) {
