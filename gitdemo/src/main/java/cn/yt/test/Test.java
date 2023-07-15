9f4ff9166817627ab1e136a0d0b90a929b6d2b44
denghongda <1990995390@qq.com>
2023-07-16 00:38:08 +0800

    hah

--- gitdemo/src/main/java/cn/yt/test/Test.java
+++ gitdemo/src/main/java/cn/yt/test/Test.java
@@ -8,6 +8,7 @@ public class Test {
         System.out.println("hah");
         System.out.println("我是第二个创建的");
         System.out.println("我是第二个创建的");
+        System.out.println("hi dada2");
     }
 
     public static void main(String[] args) {
